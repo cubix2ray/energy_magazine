3425cc19eb86cd962c8d1651560e5b80a8b10e2e
cubix2ray <cubix2ray@gmail.com>
2020-04-24 19:56:09 +0200

    comments added

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'current_load = energy_magazine.current_load:main',
+            'current_load = ' + package_name + '.current_load:main',
         ],
     },
 )
